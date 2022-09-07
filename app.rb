63af93be5f4ec990bbb2cf871ef557a37d76d98d
Adrian Fee <adrian.fee@yapily.com>
2022-09-07 20:12:09 +0700

    spaces listed and table with check in and out dates, including submit booking button. booking not going through to database

--- app.rb
+++ app.rb
@@ -2,16 +2,32 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 require_relative 'lib/database_connection'
 require_relative 'lib/booking_repository'
-require_relative 'lib/booking'
+require_relative 'lib/list_spaces_repository'
+
+DatabaseConnection.connect
 
 class Application < Sinatra::Base
   # This allows the app code to refresh
   # without having to restart the server.
   configure :development do
     register Sinatra::Reloader
+    also_reload 'lib/booking_repository'
+    also_reload 'lib/list_spaces_repository'
   end
 
   get '/bookings/new' do
+    @spaces = SpaceRepository.new 
+    @space = Space.new 
+    @booking_repo = BookingRepository.new
+    @booking = Booking.new
+    @booking.start_date = params[:start_date]
+    @booking.end_date = params[:end_date]
+    @booking.space_id = params[:space_id]
+    @booking.user_id = params[:user_id]
+    @booking_repo.create(@booking)
+    @space.name = params[:name]
+    @space.availability = params[:availability]
+    @space.owner_id = params[:owner_id]
     return erb(:new_booking)
   end
 
@@ -20,3 +36,4 @@ class Application < Sinatra::Base
   end 
 
 end
+
