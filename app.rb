c783413d5a610f11758aeac176ef3877647a1d46
Joshua Perrin <joshuaperrin101@gmail.com>
2022-09-07 16:35:32 +0100

    Added Post bookings/available test, html and methods

--- app.rb
+++ app.rb
@@ -2,18 +2,28 @@ require 'sinatra/base'
 require 'sinatra/reloader'
 require_relative 'lib/database_connection'
 require_relative 'lib/booking_repository'
+require_relative 'lib/list_spaces_repository'
 require_relative 'lib/booking'
+
+DatabaseConnection.connect
 
 class Application < Sinatra::Base
   # This allows the app code to refresh
   # without having to restart the server.
   configure :development do
     register Sinatra::Reloader
+    also_reload 'lib/booking_repository'
+    also_reload 'lib/list_spaces_repository'
   end
 
   get '/bookings/new' do
     return erb(:new_booking)
   end
+
+  post '/bookings/available' do
+    @spaces = SpaceRepository.new
+    return erb(:available_bookings)
+  end 
 
   post '/bookings' do 
     return erb(:confirmation_booking)
--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -14,6 +14,23 @@ describe Application do
             expect(response.status).to eq(200)
             expect(response.body).to include('<h1>Make a new booking request</h1>')
             expect(response.body).to include('User ID: <input type="text" name="user_id">')
+            expect(response.body).to include('Check-in date: <input type="date" name="start_date"')
+            expect(response.body).to include('Check-out date: <input type="date" name="end_date"')
+            expect(response.body).to include('<input type="submit" value="Search">')
+        end
+    end
+
+    context 'POST /bookings/available' do
+        it 'returns dates once search sent' do
+            response = post('/bookings/available',
+                start_date: '2022-09-04', 
+                end_date: '2022-09-10')
+            
+            expect(response.status).to eq(200)
+            expect(response.body).to include('<h1>Available Spaces</h1>')
+            expect(response.body).to include('<td>Beach house</td>')
+            expect(response.body).to include('<td>Mountain view</td>')
+            expect(response.body).to include('<input type="submit" value="Book now!">')
         end
     end
 
